89560ae347e87238f594865c9556cda21b3699e2
Aaron Bockover <abockover@novell.com>
2009-07-28 07:44:46 -0400

    Added proper command line support, createrepo

--- RpmRepoClone/RpmRepoClone/Entry.cs
+++ RpmRepoClone/RpmRepoClone/Entry.cs
@@ -2,24 +2,115 @@ using System;
 using System.IO;
 using System.Linq;
 using System.Collections.Generic;
+using System.Diagnostics;
 
 using Banshee.IO;
+using Mono.Options;
 
 namespace RpmRepoClone
 {
     public static class Entry
     {
-        public static void Main ()
+        private static List<string> architectures = new List<string> ();
+        private static List<Uri> repositories = new List<Uri> ();
+        private static string ssh_rsync_destination;
+        private static bool create_repo;
+    
+        public static void Main (string [] args)
         {
-            Console.WriteLine ("Loading remote repository data...");
+            try {
+                Startup (args);
+            } catch (OptionException e) {
+                Console.Error.Write ("rpm-repo-clone: ");
+                Console.Error.WriteLine (e.Message);
+                Console.Error.WriteLine ("Try --help for more information");
+                Environment.Exit (1);
+            } catch (Exception e) {
+                Console.Error.Write ("rpm-repo-clone: ");
+                Console.Error.WriteLine (e.Message);
+                Environment.Exit (1);
+            }
+            
+            foreach (var repo in repositories) {
+                ProcessRepository (repo);
+            }
+            
+            if (create_repo) {
+                CreateRepodata ();
+            }
+            
+            Console.WriteLine ("Done.");
+        }
+        
+        private static void Startup (string [] args)
+        {
+            bool show_help = false;
+            string alternate_pwd = null;
+            
+            var options = new OptionSet () {
+                { "a|arch=", "add an architecture (default is i586)", v => architectures.Add (v) },
+                { "d|dir=", "use an alternative working/output directory", v => alternate_pwd = v },
+                { "c|createrepo", "run createrepo when finished", v => create_repo = v != null },
+                { "s|sync=", "run ssh rsync when finished", v => ssh_rsync_destination = v },
+                { "h|help", "show this message and exit", v => show_help = v != null }
+            };
+            
+            foreach (var uri in options.Parse (args)) {
+                repositories.Add (new Uri (uri, UriKind.Absolute));
+            }
+            
+            if (show_help) {
+                Console.Error.WriteLine ("Usage: rpm-repo-clone OPTIONS+ <repository> [<repositories>]");
+                Console.Error.WriteLine ();
+                Console.Error.WriteLine ("Options:");
+                options.WriteOptionDescriptions (Console.Error);
+                Environment.Exit (1);
+            }
+            
+            if (alternate_pwd != null) {
+                if (!Directory.Exists (alternate_pwd)) {
+                    throw new DirectoryNotFoundException ("Invalid working directory: " + alternate_pwd);
+                }
+                
+                Environment.CurrentDirectory = alternate_pwd;
+            }
+            
+            if (repositories.Count == 0) {
+                throw new ApplicationException ("One or more repositories must be specified");
+            }
+            
+            if (architectures.Count == 0) {
+                architectures.Add ("i586");
+            }
+            
+            Console.WriteLine ("Session Configuration:");
+            Console.WriteLine ();
+            
+            Console.WriteLine ("  Repositories:");
+            foreach (var repo in repositories) {
+                Console.WriteLine ("    {0}", repo);
+            }
+            
+            Console.WriteLine ();
+            Console.WriteLine ("  Architectures:");
+            foreach (var arch in architectures) {
+                Console.WriteLine ("    {0}", arch);
+            }
+            
+            Console.WriteLine ();
+            Console.WriteLine ("  Working directory: {0}", Environment.CurrentDirectory);
+            Console.WriteLine ("  Create repodata:   {0}", create_repo ? "yes" : "no");
+            Console.WriteLine ("  SSH rsync to:      {0}", ssh_rsync_destination ?? "<skip>");
+            Console.WriteLine ();
+        }
+        
+        private static void ProcessRepository (Uri repository)
+        {
+            Console.WriteLine ("Loading repo [{0}]...", repository);
             var remote_packages = new RpmMetadataDocument () {
-                BasePackageUri = "http://dist.suse.de/ibs/Devel://Moblin://Factory/SUSE_SLE-11_GA/"
+                BasePackageUri = repository.AbsoluteUri
             };
             remote_packages.Load ();
-    
-            var architectures = new List<string> () {
-                "i586"
-            };
             
             foreach (var arch in architectures) {
                 Console.WriteLine ("Updating packages for {0}...", arch);
@@ -100,6 +191,16 @@ namespace RpmRepoClone
                 File.SetLastWriteTime (package.RelativeLocation, package.FileTime);
                 File.SetLastAccessTime (package.RelativeLocation, package.FileTime);
             }
+        }
+        
+        private static void CreateRepodata ()
+        {
+            Console.WriteLine ("Creating repodata...");
+            Directory.CreateDirectory ("repodata");
+            var proc = Process.Start (new ProcessStartInfo (
+                "createrepo", String.Format ("-o . -p .")));
+            proc.Start ();
+            proc.WaitForExit ();
         }
     }
 }
